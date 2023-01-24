2e778fc910c2f27252cd2e8fdd103fd7ad766563
pietrobolcato <pietrobolcato@gmail.com>
2023-01-24 16:19:28 -0600

    feat: added automatic deployment v1

---
+++ cloud/deployment/deploy.sh
@@ -0,0 +1,7 @@
+echo "- Creating model..."
+az ml model create -f model.yaml
+echo "- Model created succesfully"
+
+echo "- Creating endpoint..."
+az ml online-endpoint create -f endpoint.yaml
+echo "- Endpoint created succesfully"
