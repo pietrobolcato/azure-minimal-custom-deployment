a43bbcfdaec6964b0b2438279e02354ea9c6d490
pietrobolcato <pietrobolcato@gmail.com>
2023-01-25 10:37:08 -0600

    feat: updated deploy.sh to check if endpoint already exist

--- cloud/deployment/cicd/deploy.sh
+++ cloud/deployment/cicd/deploy.sh
@@ -1,7 +1,20 @@
+ENDPOINT="mnist-endpoint-865021"
+
 echo "- Creating model..."
 az ml model create -f ../model.yaml
 echo "- Model created succesfully"
 
-echo "- Creating endpoint..."
-az ml online-endpoint create -f ../endpoint.yaml
-echo "- Endpoint created succesfully"
+echo "- Checking if endpoint exists already"
+
+if [[ $(az ml online-endpoint list --query "[?name=='$ENDPOINT'] | length(@)") > 0 ]]
+then
+    echo "- Endpoint exists"
+else
+    echo "- Endpoint doesn't exist"
+    echo "- Creating endpoint..."
+
+    az ml online-endpoint create -f ../endpoint.yaml
+    
+    echo "- Endpoint created succesfully"
+fi
+
