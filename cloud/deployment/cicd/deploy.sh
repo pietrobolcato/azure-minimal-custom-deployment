4155b20c22e8cce085afa9cb60dcf7595b391626
pietrobolcato <pietrobolcato@gmail.com>
2023-01-25 10:21:15 -0600

    fix: deployment script in cicd

--- cloud/deployment/cicd/deploy.sh
+++ cloud/deployment/cicd/deploy.sh
@@ -3,5 +3,5 @@ az ml model create -f ../model.yaml
 echo "- Model created succesfully"
 
 echo "- Creating endpoint..."
-az ml online-endpoint create -f endpoint.yaml
+az ml online-endpoint create -f ../endpoint.yaml
 echo "- Endpoint created succesfully"
